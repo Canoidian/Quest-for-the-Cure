48f562e31fe21c9639c81b3561f5d0c923fac9d1
William <eh.canoidian@gmail.com>
2023-06-24 17:55:24 -0400

    wall jump not working

---
+++ player.gd
@@ -0,0 +1,76 @@
+extends CharacterBody2D
+
+@export var movement_data : PlayerMovementData
+
+var air_jump = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	handle_wall_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	apply_air_resistance(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
+	if input_axis !=0:
+		velocity.x = input_axis * movement_data.speed
+	else:
+		velocity.x = 0
+		
+	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	
+	if just_left_ledge:
+		coyote_jump_timer.start()
+	
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * movement_data.gravity_scale * delta
+		
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta)
+		
+func handle_wall_jump():
+	if not is_on_wall(): return
+	var wall_normal = get_wall_normal()
+	if Input.is_action_just_pressed("ui_left") and wall_normal == Vector2.LEFT:
+		velocity.x = wall_normal.x * movement_data.speed
+		velocity.y = movement_data.jump_velocity
+	if Input.is_action_just_pressed("ui_right") and wall_normal == Vector2.RIGHT:
+		velocity.x = wall_normal.x * movement_data.speed
+		velocity.y = movement_data.jump_velocity
+
+func handle_jump():
+	if is_on_floor(): air_jump = true
+	
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = movement_data.jump_velocity
+	if not is_on_floor():
+		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 2:
+			velocity.y = movement_data.jump_velocity / 2
+			
+		if Input.is_action_just_pressed("ui_up") and air_jump:
+			velocity.y = movement_data.jump_velocity * 0.8
+			air_jump = false
+			
+func apply_air_resistance(input_axis, delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
+			
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+		
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
---
+++ world.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+
+func _ready():
+	RenderingServer.set_default_clear_color(Color.DARK_RED)
+	polygon_2d.polygon = collision_polygon_2d.polygon
+
