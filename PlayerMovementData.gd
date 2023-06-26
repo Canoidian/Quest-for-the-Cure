1b2d826e514c63f072b111dbd759e2246c60346f
William <eh.canoidian@gmail.com>
2023-06-26 15:23:15 -0400

    Heart Pick up error

--- PlayerMovementData.gd
+++ PlayerMovementData.gd
@@ -3,6 +3,6 @@ extends Resource
 
 @export var speed = 100.0
 @export var jump_velocity = -300.0
-@export var gravity_scale = 1.0
+@export var gravity_scale = 0.8
 @export var air_resistance = 200.0
 @export var air_acceleration = 400.0
